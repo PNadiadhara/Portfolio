d61eb4c8978752e00a2884dc2d4206067dd11993
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2021-07-05 21:12:49 -0400

    lighting effects

--- main.js
+++ main.js
@@ -15,3 +15,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera)
+
+//https://threejs.org/docs/index.html?q=geo#api/en/geometries/BoxGeometry
+const geometry = new THREE.BoxGeometry(7,11,2);
+const material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+//https://threejs.org/docs/index.html?q=point#api/en/lights/PointLight
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5)
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper)
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.005;
+  cube.rotation.z += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate()
